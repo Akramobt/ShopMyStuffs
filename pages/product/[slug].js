cba87e592db396fa0a715acf293748c8ea7d6b64
Akramobt <39308624+Akramobt@users.noreply.github.com>
2022-11-21 22:35:50 +0100

    Adding slug Id

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import NextLink from 'next/link';
+import { Link } from '@mui/material';
+import {useRouter} from 'next/router';
+import data from '../../utils/data';
+import Layout from '../../components/Layout';
 
 export default function ProductScreen() {
+ const router= useRouter();
+ const{slug}= router.query;
+ const product=data.product.find((a) => a.slug === slug);
+
+ if(!product){ 
+   return <div> Product not found</div>;
+ }
   return (
-    <div> 
-        <h1>Product Screen</h1>
-    </div>
-  )
+    <Layout title = {product.name}>
+      <div>
+        <NextLink>
+          <Link>back to product</Link>
+        </NextLink>
+      </div>
+    </Layout>
+    
+  );
+  
 }
