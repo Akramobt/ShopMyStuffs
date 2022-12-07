ff459ddabbd1489b7289cdee9aa0c520ab3eb656
Akramobt <39308624+Akramobt@users.noreply.github.com>
2022-12-07 16:14:57 +0100

    this Connect Branch but with errors

--- model/Product.js
+++ model/Product.js
@@ -1,20 +1,57 @@
 import mongoose from 'mongoose';
 
-const productSchema =  new mongoose.Schema({ 
-    name: { type: String, required: true},
-    slug: { type: String, required: true, unique: true},
-    categorry: { type: String, required: true},
-    image: { type: String, required: true},
-    price: { type: Number, required: true},
-    brand: { type: String, required: true},
-    rating: { type: Number, required: true, default : 0},
-    numReview: { type: Number, required: true,default : 0},
-    countInStock: { type: Number, required: true,default : 0},
-    description: { type: String, required: true},
-    
-},{
-    timestamps:true
-});
+const productSchema = new mongoose.Schema(
+  {
+    name:{ 
+       type: String, 
+        required: true 
+    },
+    slug:{ 
+        type: String,
+         required: true, 
+         unique: true 
+    },
+    categorry:{ 
+        type: String, 
+        required: true 
+    },
+    image:{ 
+        type: String, 
+        required: true 
+    },
+    price:{ 
+        type: Number, 
+        required: true 
+    },
+    brand:{ 
+        type: String, 
+        required: true 
+    },
+    rating:{ 
+        type: Number, 
+        required: true, 
+        default: 0 
+    },
+    numReview:{ 
+        type: Number, 
+        required: true, 
+        default: 0 
+    },
+    countInStock:{ 
+        type: Number, 
+        required: true, 
+        default: 0 
+    },
+    description:{ 
+        type: String, 
+        required: true 
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Product = mongoose.models.Product || mongoose.model('Product', productSchema);
+const Product =
+  mongoose.models.Product || mongoose.model('Product', productSchema);
 export default Product;
